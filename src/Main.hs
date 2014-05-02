b5e57681d79d2141ccbbc4a13969a3cc3e288f72
Chathura Colombage <dcdewaka@gmail.com>
2014-05-03 00:11:55 +0100

    Command line Hacker News reader work started

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,47 @@
 -- Starting point
 
+import System.Cmd
+
+hnRss = "https://news.ycombinator.com/rss"
+
+-- As a start write a tool to read Hacker news on the command line
+-- Following would be an example
+-- <?xml version="1.0" encoding="UTF-8"?>
+-- <rss version="2.0">
+--    <channel>
+--       <title>Hacker News</title>
+--       <link>https://news.ycombinator.com/</link>
+--       <description>Links for the intellectually curious, ranked by readers.</description>
+--       <item>
+--          <title>How Steve Wozniak Wrote BASIC for the Original Apple From Scratch</title>
+--          <link>http://gizmodo.com/how-steve-wozniak-wrote-basic-for-the-original-apple-fr-1570573636/all</link>
+--          <comments>https://news.ycombinator.com/item?id=7687174</comments>
+--          <description><![CDATA[<a href="https://news.ycombinator.com/item?id=7687174">Comments</a>]]></description>
+--       </item>
+--     </channel>
+-- </rss>
+
+data HackerNewsItem = HackerNewsItem { title :: String
+                                     , link :: String
+                                     , comments :: String
+                                     , description :: String
+                                     } deriving (Show, Eq)
+
+openUrlInBrowser cmd = system openCmd
+  where openCmd = "open " ++ cmd
+
+bogusNews = [ HackerNewsItem { title = "abc"
+                             , link = "https://www.google.com"
+                             , comments = "abc comments"
+                             , description = "abc description" }
+            , HackerNewsItem { title = "def"
+                             , link = "https://www.bing.com"
+                             , comments = "def comments"
+                             , description = "def description" }
+            ]
+
+printNews :: [HackerNewsItem] -> IO ()
+printNews = mapM_ (putStrLn . show) . zip [1..] . map title
+
 main = do
   putStrLn "Starting HTrapIt"
--- src/Scraper/Scraper.hs
+++ src/Scraper/Scraper.hs
@@ -1,8 +1,14 @@
 module Scraper where
 
 import Network.Curl (curlGetString)
+import Network.Curl.Code
 
+type PageProcessor = (String -> Bool)
+type ErrorProcessor = (CurlCode -> Bool)
+
+processPage :: String -> PageProcessor -> ErrorProcessor -> IO Bool
 processPage url onSuccess onFail = do
-  (_, page) <- curlGetString url []
-  onSuccess page
-   
+  (status, page) <- curlGetString url []
+  case status of
+    CurlOK -> return $ onSuccess page
+    _ -> return $ onFail status
